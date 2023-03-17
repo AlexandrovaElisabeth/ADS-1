4737647c94b0e45bd58ba5c4f4ff8e740d9f12c4
AlexandrovaElisabeth <119954939+AlexandrovaElisabeth@users.noreply.github.com>
2023-03-17 21:32:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,44 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value <= 1) {
+        return false;
+    }
+    for (uint64_t i = 2; i < value - 1; i++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    std::vector<int> primes;
+    primes.push_back(2);
+    uint64_t num = 3;
+    while (primes.size() < n) {
+        if (isPrime(num)) {
+            primes.push_back(num);
+        }
+        num += 2;
+    }
+    return primes.back();
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    value+=1;
+    while (!isPrime(value)) {
+        value++;
+    }
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t sum = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (isPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
 }
