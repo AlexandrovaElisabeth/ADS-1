e98d75dec13059da9b37aecd3f749dde9b92aae7
AlexandrovaElisabeth <119954939+AlexandrovaElisabeth@users.noreply.github.com>
2023-03-17 21:43:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,46 +2,45 @@
 #include <cstdint>
 #include "alg.h"
 
-
 bool checkPrime(uint64_t value) {
-  if (value <= 1) {
+if (value <= 1) {
+return false;
+}
+for (uint64_t i = 2; i < value - 1; i++) {
+    if (value % i == 0) {
         return false;
     }
-    for (uint64_t i = 2; i < value - 1; i++) {
-        if (value % i == 0) {
-            return false;
-        }
-    }
-    return true;
 }
-
+return true;
+}
+  
 uint64_t nPrime(uint64_t n) {
-    std::vector<int> primes;
-    primes.push_back(2);
-    uint64_t num = 3;
-    while (primes.size() < n) {
-        if (isPrime(num)) {
-            primes.push_back(num);
-        }
-        num += 2;
+std::vector<int> primes;
+primes.push_back(2);
+uint64_t num = 3;
+while (primes.size() < n) {
+    if (isPrime(num)) {
+        primes.push_back(num);
     }
-    return primes.back();
+    num += 2;
+}
+return primes.back();
 }
 
 uint64_t nextPrime(uint64_t value) {
-    value+=1;
-    while (!isPrime(value)) {
-        value++;
-    }
-    return value;
+value+=1;
+while (!isPrime(value)) {
+    value++;
+}
+return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t sum = 0;
-    for (uint64_t i = 2; i < hbound; i++) {
-        if (isPrime(i)) {
-            sum += i;
-        }
+uint64_t sum = 0;
+for (uint64_t i = 2; i < hbound; i++) {
+    if (isPrime(i)) {
+        sum += i;
     }
-    return sum;
+}
+return sum;
 }
